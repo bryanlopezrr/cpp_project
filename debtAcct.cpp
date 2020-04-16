646b599a02b95a79dd8cf80cb70df16c2f0f0b6e
Bryan Lopez <bryanlopezrr@gmail.com>
2020-04-16 18:36:39 -0400

    functioning DB connection

--- debtAcct.cpp
+++ debtAcct.cpp
@@ -3,6 +3,8 @@
 #include <vector>
 #include <string>  
 #include <sstream>
+#include <fstream>
+#include <pqxx/pqxx>
 
 using namespace std;
 
@@ -12,6 +14,26 @@ int main(){
 
     instructions();
 
+    string connection_string = "host=localhost port=5432 dbname=postgres user=postgres password=password";
+    pqxx::connection con(connection_string.c_str());
+    pqxx::work wrk(con);
+
+    // pqxx::result res = wrk.exec("SELECT * FROM debtDetails");
+
+    if(res.size() < 1){
+        cout << "An error has occurred\n";
+        return 1;
+    }
+
+    // cout << "Debt ID: \t" << "Debt Amount: \n";
+    // for (int i = 0; i < res.size(); i++)
+    // {
+    //     cout << res[i][0] << "\t\t" << res[i][1] << endl;
+    // }
+
+
+    //ADDING SWITCH STAMENT FOR THE OPTIONS: 
+
     return 0; 
 }
 
@@ -19,6 +41,6 @@ void instructions(){
     cout << "\n=======================================\n";
     cout << "      DEBT: ACCOUNTABILITY PROGRAM\n";
     cout << "=======================================\n\n";
-    cout << "Please select an option below:\n";
+    cout << "Please select an option below:\n\n";
 }
 
