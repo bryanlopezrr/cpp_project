c70e79c04831e5c7497e44be1918297a5b33a39a
Bryan Lopez <bryanlopezrr@gmail.com>
2020-04-24 22:52:11 -0400

    updates on querying and inserting into db

--- debtAcct.cpp
+++ debtAcct.cpp
@@ -22,18 +22,27 @@
 #include <string>  
 #include <sstream>
 #include <fstream>
+#include <termios.h>
+#include <unistd.h>
 #include <pqxx/pqxx>
 
 using namespace std;
 
 void instructions();
 void optionsForUsers();
+void clearScreen();
+int getch(void);
+string passwordFunction(char* password);
 
 int main(){
 
-    string connection_string = "host=localhost port=5432 dbname=postgres user=postgres password=password";
-    pqxx::connection con(connection_string.c_str());
-    pqxx::work wrk(con);
+
+    while(1){
+            //Connection string to local database - Postgres
+            string connection_string = "host=localhost port=5432 dbname=postgres user=postgres password=password";
+            pqxx::connection con(connection_string.c_str());
+            pqxx::work wrk(con);    
+
 
             //--- PROOF OF CONCEPT ----
             // pqxx::result res = wrk.exec("SELECT * FROM debtDetails");
@@ -48,13 +57,16 @@ int main(){
             //     cout << res[i][0] << "\t\t" << res[i][1] << endl;
             // }
             
-    while(1){
+
+
         //ADDING SWITCH STAMENT FOR THE OPTIONS: 
+        // clearScreen();
         instructions();
         char response = ' ';
         string username = " ", password = " "; 
         cin >> response;
 
+        char password_ch[32];
         if( toupper(response) == 'Y'){
             while(1){
                 cout << "Login in with your credentials: \n";
@@ -62,15 +74,16 @@ int main(){
                 cin >> username; 
                 cin.ignore();
 
-                char x[18];
                 cout << "Password: ";
-                cin >> password; 
-                cin.ignore();
+                // cin >> password; 
+                // cin.ignore();
+                password = passwordFunction(password_ch);    
 
                 pqxx::result res = wrk.exec("SELECT * FROM userInfo WHERE username = '" + username + "' AND passwd = '" + password + "'");
                 if(res.size() == 1){
                     char selection = ' ';
-                    cout << "\n - WELCOME BACK - \n";
+                    clearScreen();
+                    cout << "\n -- WELCOME BACK -- \n";
                     cout << "What would you like to do?:\n";            
                     optionsForUsers();
 
@@ -95,6 +108,9 @@ int main(){
                             case 'P':
                                 cout << "PAID $--- HARD CODED FOR NOW \n\n";
                                 break;
+                            case '$':
+                                cout << "MONEYYYYYY\n";
+                                break;
                             default:
                                 cout << "Incorrect input\n\n";
                                 break;
@@ -103,56 +119,65 @@ int main(){
                     }
                 }
                 else{
-                    cout << "\n - NOT FOUND - \n";
+                    clearScreen();
+                    cout << "\n -- NOT FOUND -- \n";
                     break;
                 }
                 cout << "Logging out " + username << endl;
+                sleep(3);
+                clearScreen();
                 break;
             }
         }  
         else{
+            clearScreen();
+            string fullName =" ";
+            /*
             cout << "Goodbye \n";
-            break;
-        //------SECTION TO ADD A USER INTO THE DB --- IN PROGRESS ....
-        //Only the group will need a login account but a the users can share this account ---->
-            // cout << "Lets create you an account:\n";
-            // cout << "Which group do you want to join?\n";
+            break;*/
+            cout << "\n=======================================\n" 
+                 << "          CREATE AN ACCOUNT:            \n"
+                 <<"=======================================\n"; 
+                 
+            cout << "Enter first name, last name \n";
+            getline(cin, fullName);
+            cin.ignore(1000, '\n');
+    
+            cout << "Choose a username: ";
+            cin >> username; 
+            cin.ignore(1000, '\n');
+            //Logic to check if user name exists
+            while(1){
+                pqxx::result res = wrk.exec("SELECT * FROM userInfo WHERE username = '" + username + "'");
+                if(res.size() == 1)
+                {
+                    cout << "User already exists. Try a different one\n";
+                    cin >> username;
+                    cin.ignore(1000, '\n');
+                }
+                else{
+                    break;
+                }
+            }
+            cout << "Type password: ";
+            password = passwordFunction(password_ch); 
+            // cin >> password; 
+            // cin.ignore(1000, '\n');
 
-            //     while (true)
-            //     {
-            //         cout << "Press the name of the group\n";
-            //         string groupName;
-            //         getline(cin, groupName);
+            //if query is succesful == return Account succesful and go back to the top 
+            //else some error occurred exit program 
+            
+            pqxx::result res = wrk.exec("INSERT INTO userinfo(username, passwd, \"userID\") VALUES('" + username + "', '" + password + "', 7372)");
+            wrk.commit();
 
-            //         if (groupName == "Win") // we will use database to get the Name of group in data by using hash table or other methods from SQL
-            //         {
-            //             cout << "You are added to a new group....\n";
-            //             break;
-            //         }
-            //         else
-            //         {
-            //             cout << "The name of the group does not exist\n Please choose another name or you want to exit the program!\n";
-            //             cout << "Press 'yes' to choose another name, press 'no' to exit the program\n";
-            //             string check;
-            //             getline(cin, check);
-            //             cover(check);
-            //             while (check != "yes" && check != "no")
-            //             {
-            //                 cout << "Please press 'yes' to agree, and 'no' to exit the program\n";
-            //                 getline(cin, str);
-            //                 cover(check);
-            //             }
-            //             if (check == "no")
-            //             {
-            //                 cout << "Exit the program\n";
-            //                 return 0;
-            //             }
-            //             else
-            //             {
-            //                 continue;
-            //             }
-            //         }
-            //     }
+            if(res.size() == 0){
+                cout << "\n *** ACCOUNT CREATED ^_^ ***\n";
+            }
+            else{
+                cout << "\nAn error occurred. Try again\n";
+            }
+            sleep(3);
+            clearScreen();
         // ............................................................
         } 
     }
@@ -171,5 +196,47 @@ void instructions(){
 void optionsForUsers(){
     cout << "\n================================================\n";
     cout << "| V - View balance | P - Paid amount | X - Exit |\n";
+    cout << "| ---------------- | --------------- | -------- |\n";
+    cout << "| $ - All payments |                 |          |\n";
     cout << "=================================================\n";
 }
+
+void clearScreen(){
+    //not secure to use system() but for demo purposes, its cleaner
+    system("clear");
+}
+
+string passwordFunction(char* password){
+        char temp =' '; 
+        string strPassword = "";
+
+        for(int i=0;;){
+            temp = getch();
+            strPassword.push_back(temp);
+            if((temp >= 'a' && temp <= 'z') || (temp >= 'A' && temp <= 'Z') || (temp >= '0' && temp <= '9')){
+                password[i] = temp;
+                        ++i; 
+                        cout << "*";
+            }
+            else if (temp == '\n'){
+                break;
+            }
+        }
+        strPassword.pop_back();
+        return strPassword;
+}
+
+int getch(void){
+	struct termios oldt, newt;
+	int ch;
+
+	tcgetattr(STDIN_FILENO, &oldt);
+	newt = oldt;
+	newt.c_lflag &= ~(ICANON | ECHO);
+	tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+	ch = getchar();
+	tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+
+	return ch;
+}
+
